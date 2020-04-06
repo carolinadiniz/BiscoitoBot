78fe6c0f6e1299ce5b0e327af3edcc18a4781d50
Carolina Diniz (Kaworii) <phdsd.phdsd@gmail.com>
2020-04-06 14:04:36 -0300

    moving commands from app.js to commands.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const { prefix, token } = require('./config/config.json')
-const Jimp = require('./jimp')
+const commands = require('./commands/commands')
 
 client.on('ready', () => {
    console.log(`Logged in as ${client.user.tag}!`)
@@ -16,28 +16,8 @@ client.on('message', async message => {
    if (message.channel.type === "dm") return;
    if (!message.content.startsWith(prefix)) return;
 
-   const args = message.content.slice(prefix.length).trim().split(/ +/g)
-   const command = args.shift().toLowerCase()
 
-   console.log(message.content)
-
-   // PING COMMAND
-   if (command === 'ping') {
-      message.reply('pong')
-   }
-
-   if (message.content.slice(prefix.length) === 'oi biscoito') {
-      message.reply(`oioi!`)
-   }
-
-   if (message.content.slice(prefix.length) === 'estão me ignorando') {
-      message.reply(`sempre fazem isso, mas já já eu compro só biscoito pra você ^^ <3`)
-   }
-
-   // AVATAR COMMAND
-   if (command === 'avatar') {
-      Jimp(client, message)
-   }
+   commands(client, message, prefix)
    
 })
 

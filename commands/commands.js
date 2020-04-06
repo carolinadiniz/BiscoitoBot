6b8eb66e677c915d4ebab1c95170ef50579fb5ab
Carolina Diniz (Kaworii) <phdsd.phdsd@gmail.com>
2020-04-06 14:04:10 -0300

    adding commands from app.js

--- commands/commands.js
+++ commands/commands.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-   console.log('oi')
+const Jimp = require('../jimp')
+
+module.exports = (client, message, prefix) => {
+   const args = message.content.slice(prefix.length).trim().split(/ +/g)
+   const command = args.shift().toLowerCase()
+
+
+   // PING COMMAND
+   if (command === 'ping') {
+      message.reply('pong')
+   }
+
+   // AVATAR COMMAND
+   if (command === 'avatar') {
+      Jimp(client, message)
+   }
 }
